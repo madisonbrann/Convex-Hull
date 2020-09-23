0f760046958b0c1dbae874c893f24a87eb231252
foreign-skies <56491559+foreign-skies@users.noreply.github.com>
2020-09-23 14:08:32 -0600

    Debugging FInal Result

--- proj2/convex_hull.py
+++ proj2/convex_hull.py
@@ -10,6 +10,7 @@ else:
 
 import time
 import math
+from functools import partial
 
 
 # Some global color constants that might be useful
@@ -67,7 +68,6 @@ class ConvexHullSolver(QObject):
 	def mergeSort(self, points,l_index,mid_index,r_index):
 		l_arr_size = mid_index - l_index + 1
 		r_arr_size = r_index - mid_index
-		print(l_arr_size)
 		left_arr = []
 		right_arr = []
 		for ini in range(0,l_arr_size):
@@ -112,22 +112,97 @@ class ConvexHullSolver(QObject):
 		if (len(points) == 1):
 			return points
 		
-		l_half = self.divide_and_conquer(points[0: math.floor(len(points) / 2)])
-		r_half= self.divide_and_conquer(points[math.floor(len(points) / 2):])
+		l_half = self.divide_and_conquer(points[0: len(points) // 2])
+		r_half= self.divide_and_conquer(points[len(points) // 2:])
 
 		return self.merge(l_half,r_half)
 	
 	def merge(self,l_half,r_half):
-		print("yooo")
 		if (len(l_half) + len(r_half) < 4):
 			x,y = self.get_center(l_half + r_half)
+			output1 = sorted(l_half + r_half, key=partial(self.clockwise, x_point = x, y_point = y))
+			return output1
+		left_beg = self.closest_point(l_half,"l")
+		right_beg = self.closest_point(r_half,"r")
+		l_top_tan,r_top_tan = self.get_up_tan(left_beg,l_half,right_beg,r_half)
+		r_bottom_tan,l_bottom_tan = self.get_up_tan(right_beg,r_half,left_beg,l_half)
+		output2 = []
+		output2.append(l_half[l_top_tan])
+		index = r_top_tan
+		while r_half[index % len(r_half)] != r_half[r_bottom_tan]:
+			output2.append(r_half[index % len(r_half)])
+			index += 1
+		output2.append(r_half[r_bottom_tan])
+		index = l_bottom_tan
+		while l_half[index % len(l_half)] != l_half[l_top_tan]:
+			output2.append(l_half[index % len(l_half)])
+			index += 1
+		return output2
+		
 
+
+	def get_up_tan(self, left_beg, l_half,right_beg,r_half):
+		l_index = left_beg
+		r_index = right_beg
+		prev_tan = tuple([l_index,r_index])
+		while True:
+
+			slope = self.find_slope(l_half[(l_index - 1) % len(l_half)],r_half[r_index])
+			left_decreasing = True
+			while left_decreasing == True:
+				slope2 = self.find_slope(l_half[(l_index - 1) % len(l_half)], r_half[r_index])
+
+				if slope2 < slope:
+					slope = slope2
+					l_index -= 1
+					if l_index < 0:
+						l_index = len(l_half) - 1
+					left_decreasing = True
+				else:
+					left_decreasing = False
+
+			right_increasing = True
+			while right_increasing == True:
+				slope2 = self.find_slope(l_half[l_index],r_half[(r_index + 1) % len(r_half)])
+				if slope2 > slope:
+					slope = slope2
+					r_index += 1
+					if r_index >= len(r_half):
+						r_index %= len(r_half)
+					right_increasing = True
+				else:
+					right_increasing = False
+			current_tan = tuple([l_index,r_index])
+			if prev_tan == current_tan:
+				break
+			prev_tan = current_tan
+		return l_index,r_index
+
+	def find_slope(self,left, right):
+		output = (right.y() - left.y())/(right.x() - left.x())
+		return output
+
+	def closest_point(self,points,side):
+		index = 0
+		if side == "l":
+			for i, point in enumerate(points):
+				if point.x() > points[index].x():
+					index = i
+		if side == "r":
+			for i, point in enumerate(points):
+				if point.x() < points[index].x():
+					index = i
+		return index
+		
+	def clockwise(self,point, x_point, y_point, anchor="ne"):
+		output = math.atan2(point.x() - x_point, point.y() - y_point)
+		return output
 	def get_center(self, points):
 		x = [point.x() for point in points]
-		y = [point.y for point in points]
+		y = [point.y() for point in points]
 		x = sum(x)/len(points)
 		y = sum(y)/len(points)
-		print("YOOO")
+
 		return x,y
 
 
@@ -143,15 +218,14 @@ class ConvexHullSolver(QObject):
 		t1 = time.time()
 	
 		# TODO: SORT THE POINTS BY INCREASING X-VALUE
-		print(points)
 		self.mySort(points, 0, len(points) - 1)
-		print("sorted")
-		print(points)
 		t2 = time.time()
 
 		t3 = time.time()
 		# this is a dummy polygon of the first 3 unsorted points
 		polygon = self.divide_and_conquer(points)
+		print("this happened")
+		print(polygon)
 		# TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
 		t4 = time.time()
 
